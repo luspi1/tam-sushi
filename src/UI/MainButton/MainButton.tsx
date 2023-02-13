a9c8b2b01859e418a1ad98eacc6d684b1106fd19
luspi1 <47425618+luspi1@users.noreply.github.com>
2023-02-13 17:02:51 +0300

    правки типа

--- src/UI/MainButton/MainButton.tsx
+++ src/UI/MainButton/MainButton.tsx
@@ -2,7 +2,7 @@ import React, { FC, ReactNode } from 'react'
 import styles from './style.module.css'
 interface IMainButton {
 	children: string | ReactNode,
-	onClick: () => void
+	onClick?: () => void
 }
 const MainButton: FC<IMainButton> = ({children, onClick}) => {
 	return (
